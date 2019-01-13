2fa7ce0ac4d03e2022a85afedac764ddfe3d9c82
Dmitry V. Sokolov <ha@haqr.eu>
2019-01-13 01:06:45 +0100

    proper vao/vbo alloc/dealloc

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,16 @@
 #include "geometry.h"
 #include "model.h"
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-const GLuint WIDTH = 800, HEIGHT = 600;
-GLuint prog_hdlr;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (GLFW_RELEASE == action) {
+        return;
+    }
+    std::cerr << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
 
 void read_n_compile_shader(const char *filename, GLuint &hdlr, GLenum shaderType) {
     std::cerr << "Loading " << filename << "... ";
@@ -75,14 +82,7 @@ void set_shaders(GLuint &prog_hdlr, const char *vsfile, const char *fsfile) {
     }
 }
 
-int main(int argc, char** argv) {
-    if (argc<2) {
-        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
-        return -1;
-    }
-
-    Model model(argv[1]);
-
+int setup_window(GLFWwindow* &window, const GLuint width, const GLuint height) {
     std::cerr << "Starting GLFW context, OpenGL 3.3" << std::endl;
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -90,11 +90,10 @@ int main(int argc, char** argv) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    window = glfwCreateWindow(width, height, "OpenGL starter pack", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
         return -1;
     }
 
@@ -105,9 +104,25 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    glViewport(0, 0, WIDTH, HEIGHT);
-    set_shaders(prog_hdlr, "../shaders/vertex.glsl", "../shaders/fragment.glsl");
+    return 0;
+}
 
+int main(int argc, char** argv) {
+    if (argc<2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return -1;
+    }
+    Model model(argv[1]);
+
+    const GLuint width = 1024, height = 768;
+    GLFWwindow* window;
+    if (setup_window(window, width, height)) {
+        glfwTerminate();
+        return -1;
+    }
+
+    GLuint prog_hdlr;
+    set_shaders(prog_hdlr, "../shaders/vertex.glsl", "../shaders/fragment.glsl");
 
     std::vector<GLfloat> vertices(3*model.nverts());
     for (int i=0; i<model.nverts(); i++) {
@@ -122,66 +137,58 @@ int main(int argc, char** argv) {
          }
     }
 
+    // create the VAO that we use when drawing
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao); // allocate and assign a Vertex Array Object to our handle
+    glBindVertexArray(vao);     // bind our Vertex Array Object as the current used object
 
-  GLuint rib_vbo = 0;
-  glGenBuffers(1, &rib_vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, rib_vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices.size(), vertices.data(), GL_STATIC_DRAW);
+    GLuint vertexbuffer = 0;
+    glGenBuffers(1, &vertexbuffer);              // allocate and assign one Vertex Buffer Object to our handle
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer); // bind our VBO as being the active buffer and storing vertex attributes (coordinates)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices.size(), vertices.data(), GL_STATIC_DRAW); // copy the vertex data to our buffer. The buffer contains sizeof(GLfloat) * 3 * nverts bytes
 
-  GLuint rib_ebo = 0;
-  glGenBuffers(1, &rib_ebo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rib_ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+    GLuint elementbuffer = 0;
+    glGenBuffers(1, &elementbuffer);                      // allocate and assign one Element Buffer Object to our handle
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer); // bind our VBO as being the active buffer and storing indices
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW); // copy the indices to our buffer. The buffer contains sizeof(GLuint) * 3 * ntriangles bytes
 
-  /* Create the VAO that we use when drawing. */
-  GLuint rib_vao = 0;
-  glGenVertexArrays(1, &rib_vao);
-  glBindVertexArray(rib_vao);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rib_ebo);
-  glBindBuffer(GL_ARRAY_BUFFER, rib_vbo);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*3, (void*)0);
-
-
-
-    glUseProgram(prog_hdlr);
+    glViewport(0, 0, width, height);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);   // accept fragment if it closer to the camera than the former one
+    glEnable(GL_CULL_FACE); // cull triangles whose normal is not towards the camera
 
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+        glUseProgram(prog_hdlr);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glBindVertexArray(rib_vao);
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // index buffer
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+
+        // draw the triangles!
         glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, (GLvoid*)0);
-        glBindVertexArray(0);
 
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-/*
-    // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-*/
-    glDeleteProgram(prog_hdlr);
+    // properly de-allocate all the resources once they have outlived their purpose
     glUseProgram(0);
+    glDeleteProgram(prog_hdlr); // note that the shader objects are automatically detached and deleted, since they were flagged for deletion by a previous call to glDeleteShader
+    glDisableVertexAttribArray(0);
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &elementbuffer);
+    glDeleteVertexArrays(1, &vao);
 
     glfwTerminate();
     return 0;
 }
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if (GLFW_RELEASE == action) {
-        return;
-    }
-    std::cerr << key << std::endl;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-}
-
-
-
 
